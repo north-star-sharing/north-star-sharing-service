9f17d1e9eb01f4f08d8cb7375157e7db76faaba6
darbach <danielarbach@gmail.com>
2021-03-24 16:08:22 -0600

    Fixed error w/method signature.

--- src/main/java/edu/cnm/deepdive/northstarsharing/model/dao/GalleryRepository.java
+++ src/main/java/edu/cnm/deepdive/northstarsharing/model/dao/GalleryRepository.java
@@ -1,11 +1,10 @@
 package edu.cnm.deepdive.northstarsharing.model.dao;
 
 import edu.cnm.deepdive.northstarsharing.model.entity.Gallery;
-import edu.cnm.deepdive.northstarsharing.model.entity.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GalleryRepository extends JpaRepository<User, UUID> {
+public interface GalleryRepository extends JpaRepository<Gallery, UUID> {
 
   Iterable<Gallery> getAllByOrderByTitleAsc();
 
